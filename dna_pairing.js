ba0c6316546a0af628b001c33153f6943ceba3a1
mats-js <matsens.mail@posteo.de>
2022-06-02 12:04:56 +0200

    feat: Added pairElement functionality

---
+++ dna_pairing.js
@@ -0,0 +1,15 @@
+
+/* Pairs of DNA strands consist of protein base pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array. */
+
+function pairElement(str) {
+    let arr = [];
+    str.split("").forEach(char => {
+        char == "G" ? arr.push(["G","C"]) : char == "C" ? arr.push(["C","G"]) : char == "A" ? arr.push(["A","T"]) : arr.push(["T","A"])
+    });
+    return arr;
+  }
+  
+  pairElement("GCG");
