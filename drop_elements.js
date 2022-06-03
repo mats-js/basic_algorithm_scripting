b3a4d8921f755e8684481ca48e77c1722fb1760c
mats-js <matsens.mail@posteo.de>
2022-06-03 15:33:17 +0200

    feat: Added dropElements functionality

---
+++ drop_elements.js
@@ -0,0 +1,11 @@
+/* Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array. */
+
+function dropElements(arr, func) {
+    while (func(arr[0]) == false) {
+        arr.splice(0, 1);
+    }
+    return arr;
+}
+
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;});
